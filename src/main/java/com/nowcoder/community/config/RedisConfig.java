2985bd33a99c052bba84c9d54397aedd059f9490
Zeroisstart4 <a1084279913@gmail.com>
2021-03-29 22:03:26 +0800

    默认变更列表

---
+++ src/main/java/com/nowcoder/community/config/RedisConfig.java
@@ -0,0 +1,37 @@
+package com.nowcoder.community.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.RedisSerializer;
+
+/**
+ * @author zhou
+ * @create 2021-3-29 20:34
+ */
+
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory factory) {
+        // 创建 Redis 模板
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        // 为 Redis 模板设置连接工厂
+        template.setConnectionFactory(factory);
+        // 设置 key 的序列化方式,String
+        template.setKeySerializer(RedisSerializer.string());
+        // 设置 value 的序列化方式,Json
+        template.setValueSerializer(RedisSerializer.json());
+        // 设置 hash 的 key 的序列化方式,String
+        template.setHashKeySerializer(RedisSerializer.string());
+        // 设置 hash 的 value 的序列化方式,Json
+        template.setHashValueSerializer(RedisSerializer.json());
+
+        template.afterPropertiesSet();
+
+        return template;
+
+    }
+}
