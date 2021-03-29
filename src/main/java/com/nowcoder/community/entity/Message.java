6e06d9355ed659f20475589b054ac1a66091131c
Zeroisstart4 <a1084279913@gmail.com>
2021-03-29 15:02:09 +0800

    默认变更列表

---
+++ src/main/java/com/nowcoder/community/entity/Message.java
@@ -0,0 +1,95 @@
+package com.nowcoder.community.entity;
+
+import java.util.Date;
+
+/**
+ * @author zhou
+ * @create 2021-3-27 14:00
+ */
+
+public class Message {
+
+    // 主键 id
+    private int id;
+    // 发送人 id
+    private int fromId;
+    // 接收人 id
+    private int toId;
+    // 会话 id
+    private String conversationId;
+    // 消息内容
+    private String content;
+    // 消息状态（是否已经查看）
+    private int status;
+    // 创建时间
+    private Date createTime;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getFromId() {
+        return fromId;
+    }
+
+    public void setFromId(int fromId) {
+        this.fromId = fromId;
+    }
+
+    public int getToId() {
+        return toId;
+    }
+
+    public void setToId(int toId) {
+        this.toId = toId;
+    }
+
+    public String getConversationId() {
+        return conversationId;
+    }
+
+    public void setConversationId(String conversationId) {
+        this.conversationId = conversationId;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    @Override
+    public String toString() {
+        return "Message{" +
+                "id=" + id +
+                ", fromId=" + fromId +
+                ", toId=" + toId +
+                ", conversationId='" + conversationId + '\'' +
+                ", content='" + content + '\'' +
+                ", status=" + status +
+                ", createTime=" + createTime +
+                '}';
+    }
+}
